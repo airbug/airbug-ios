2acf919f49d563f064082556dfbbb7d5c0b38283
Richard Shin <skunkworks@gmail.com>
2014-02-12 11:44:06 -0800

    Initial Commit

---
+++ airbug/ABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABAppDelegate.h
+//  airbug
+//
+//  Created by Richard Shin on 2/12/14.
+//  Copyright (c) 2014 airbug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ airbug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  airbug
+//
+//  Created by Richard Shin on 2/12/14.
+//  Copyright (c) 2014 airbug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABAppDelegate class]));
+    }
+}
